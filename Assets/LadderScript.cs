4df9c59bbdc17999850faae9ab57a8297fe0503c
Haikal Danung Trinanda <77687182+hkldtrinanda@users.noreply.github.com>
2022-10-22 14:38:19 +0700

    Added Climbing Feature

---
+++ Assets/LadderScript.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript : MonoBehaviour
+{
+    private float vertical;
+    public float speed;
+    private bool isClimbing;
+
+    private bool isLadder;
+    
+    public Animator anim;
+    [SerializeField] private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       anim = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+        
+        if (isLadder && Mathf.Abs(vertical) > 0f)
+        {
+            isClimbing = true;
+            anim.SetBool("isClimbing", true);
+            /*transform.Translate(Vector3.up * vertical * speed * Time.deltaTime);*/
+        }
+        else
+        {
+            isClimbing = false;
+            anim.SetBool("isClimbing", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, vertical * speed);
+        }
+        else
+        {
+            rb.gravityScale = 3f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Ladder"))
+        {
+            isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Ladder"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
+
+    }
+}
