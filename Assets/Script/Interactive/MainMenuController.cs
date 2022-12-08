18add797a73caf1630bb9674f191a4f9a6122756
coco_github <enriconabilqois.budisantoso@gmail.com>
2022-12-08 15:33:40 +0700

    Dialog Show up/Turn off + karakter diem

---
+++ Assets/Script/Interactive/MainMenuController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Scene to load")]
+    public string sceneName;
+
+    [Header("All Panel")]
+    public GameObject panelAwal;
+    public GameObject mainMenu;
+    public GameObject options;
+    public GameObject credit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mainMenu.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                mainMenu.SetActive(false);
+                panelAwal.SetActive(true);
+            }
+        }
+
+        if (options.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                options.SetActive(false);
+                mainMenu.SetActive(true);
+            }
+        }
+
+        if (credit.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                credit.SetActive(false);
+                options.SetActive(true);
+            }
+        }
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
