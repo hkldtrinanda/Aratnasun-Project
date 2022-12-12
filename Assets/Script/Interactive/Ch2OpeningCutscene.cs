f97ecc787bcc1e35f65f5aaeb915b647cc5b6cc3
Felix Kurnia Salim <felixkurnia7@gmail.com>
2022-12-12 18:43:16 +0700

    Ch2-S0OpeningCutscene selesai masuk ke Ch2S1GapuraDepan

---
+++ Assets/Script/Interactive/Ch2OpeningCutscene.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch2OpeningCutscene : MonoBehaviour
+{
+    [Header("Scene to load")]
+    public string sceneName;
+    public Animator animator;
+
+    bool loadingStarted = false;
+    float secondsLeft = 0;
+    public GameObject skipButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayLoadLevel(43));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator DelayLoadLevel(float seconds)
+    {
+        secondsLeft = seconds;
+        loadingStarted = true;
+        do
+        {
+            yield return new WaitForSeconds(1);
+            if (secondsLeft < 28)
+            {
+                skipButton.SetActive(true);
+            }
+        } while (--secondsLeft > 0);
+        animator.SetTrigger("FadeOut");
+    }
+    public void LoadNextScene()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+}
