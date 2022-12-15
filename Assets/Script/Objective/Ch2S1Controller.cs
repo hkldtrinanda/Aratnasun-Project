2fee7009325f1431816b58ac638fe4f66a80ed9f
Felix Kurnia Salim <felixkurnia7@gmail.com>
2022-12-15 11:31:05 +0700

    Add UI and fade Ch2S1-Gapura

---
+++ Assets/Script/Objective/Ch2S1Controller.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch2S1Controller : MonoBehaviour
+{
+    public float chapterBoxdelay = 0f;
+    public GameObject Chapter2box;
+    public GameObject jurnalPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Chapter2box.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chapterBoxdelay -= Time.deltaTime;
+        if (chapterBoxdelay <= 0f)
+        {
+            Chapter2box.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            jurnalPanel.SetActive(true);
+        }
+        if (jurnalPanel.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                jurnalPanel.SetActive(false);
+            }
+        }
+    }
+}
