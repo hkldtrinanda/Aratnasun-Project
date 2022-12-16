af655196ec5f02bcae0dab6e068dd2721d85e410
coco_github <enriconabilqois.budisantoso@gmail.com>
2022-12-16 10:40:14 +0700

    Add NPC Destroy

---
+++ Assets/Script/Interactive/DestroyNPC.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyNPC : MonoBehaviour
+{
+
+    public GameObject npc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "NPC")
+        {
+            Destroy(npc);
+            col.enabled = false;
+
+        }
+    }
+}
