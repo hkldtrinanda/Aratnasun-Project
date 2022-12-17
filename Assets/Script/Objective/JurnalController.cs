8e9e99b4eb6964f6f14f2ff78148971b67cd6516
Felix Kurnia Salim <felixkurnia7@gmail.com>
2022-12-17 21:40:14 +0700

    Fix dialog lintang di scene Rawa + Add pause button and jurnal button sampai Ch2S5

---
+++ Assets/Script/Objective/JurnalController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JurnalController : MonoBehaviour
+{
+    public GameObject jurnalPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            jurnalPanel.SetActive(true);
+        }
+        if (jurnalPanel.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                jurnalPanel.SetActive(false);
+            }
+        }
+    }
+}
